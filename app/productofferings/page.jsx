5a76ef97d57471c52fa5e2135b49d2a21b26756f
carlo-reyes <carlo_jose_reyes@dlsu.edu.ph>
2023-10-15 21:24:50 +0800

    Added main page for Product Offerings

---
+++ app/productofferings/page.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useState } from "react";
+//import SalesOrderList from "../components/SalesOrderList";
+//import AddProductSales from "../components/AddProductSales";
+
+const ProductOfferings = () => {
+  return (
+    <div className="w-full min-h-screen flex flex-col justify-center items-center gap-2" style={{ backgroundColor: '#27374D', color: 'white' }}>
+      <div className="w-[80%] px-10 py-4 bg-blue-400 rounded-lg" style={{ backgroundColor: '#526D82', color: 'white' }}>
+        <h1 className="font-black text-xl">Product Offerings</h1>
+      </div>
+      <div className="w-[80%] p-10 bg-blue-300 gap-6 rounded-lg" style={{ backgroundColor: '#526D82', color: 'white' }}>
+        <h3 className="text-xl font-bold">&nbsp;Name</h3>
+        <hr className="w-full border-2 mb-4" style={{ backgroundColor: '#DDE6ED', color: 'white' }}></hr>
+
+        {/* <SalesOrderList/>*/}
+
+        
+      </div>
+    </div>
+
+    
+  );
+};
+
+export default ProductOfferings;
