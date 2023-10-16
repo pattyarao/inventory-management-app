ed90f1b252cf663f175945963d2102ea302bf5d7
Zannnyyyy <raymond_intervalo@dlsu.edu.ph>
2023-10-16 15:43:33 +0800

    added record discarded materials page

---
+++ app/recordexpired/page.jsx
@@ -0,0 +1,16 @@
+import DiscardedList from "../components/DiscardedList";
+
+const RecordExpired = () => {
+  return (
+    <div className="w-full min-h-screen flex flex-col justify-center items-center gap-2" style={{ backgroundColor: '#27374D', color: 'white' }}>
+      <div className="w-[80%] px-10 py-4 bg-blue-400 rounded-lg" style={{ backgroundColor: '#526D82', color: 'white' }}>
+        <h1 className="font-black text-5xl">Record Discarded Materials</h1>
+      </div>
+      
+        <DiscardedList/>
+        
+    </div>
+  );
+};
+
+export default RecordExpired;
