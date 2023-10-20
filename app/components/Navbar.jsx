5cec5483c41bfbefaef29aabf34a1d3d1f9de682
Patty Arao <patricia_arao@dlsu.edu.ph>
2023-10-20 17:57:44 +0800

    fixed navbar layout

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -28,17 +28,19 @@ const Navbar = ({ userType }) => {
     <div className="w-full h-fit flex p-4 justify-between items-center bg-[#526D82]">
       <h3>Inventory Management</h3>
 
-      <div>
-        {userType === "Owner" ? (
-          <Link href="/assignroles">Assign Roles</Link>
-        ) : null}
-      </div>
-      <div className="flex items-center gap-6">
-        <p className="bg-[#9DB2BF] p-2 rounded-md">
-          Signed in as{" "}
-          <span className="font-bold text-[#041C32]">{userType}</span>
-        </p>
-        <button onClick={logOut}>Logout</button>
+      <div className="flex items-center gap-4">
+        <div>
+          {userType === "Owner" ? (
+            <Link href="/assignroles">Assign Roles</Link>
+          ) : null}
+        </div>
+        <div className="flex items-center gap-6">
+          <p className="bg-[#9DB2BF] p-2 rounded-md">
+            Signed in as{" "}
+            <span className="font-bold text-[#041C32]">{userType}</span>
+          </p>
+          <button onClick={logOut}>Logout</button>
+        </div>
       </div>
     </div>
   );
