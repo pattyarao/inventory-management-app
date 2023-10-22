e4bc6c512fec651b381fe8d02436c2d9be9b7b57
Patty Arao <patricia_arao@dlsu.edu.ph>
2023-10-22 12:53:56 +0800

    removed comments in page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -7,49 +7,6 @@ import withAuthentication from "../app/auth";
 import { useRouter } from "next/navigation";
 import Navbar from "./components/Navbar";
 import Dashboard from "./dashboard/page";
-// export default function Home() {
-
-//   const testFunc = async () => {
-
-//     const { data: { user }, error } = await supabase.auth.getUser()
-//     if (error) {
-//       console.error("Error signing up:", error.message);
-//       return;
-//     }
-//     console.log(user)
-//     console.log(user.id)
-
-//     const { data: userTypeData, error: userTypeError } = await supabase
-//     .from('MD_PROFILES')
-//     .select()
-//     .eq('id', user.id);
-
-//     const { data: userTypeDataInt, error: userTypeIntError } = await supabase
-//     .from('REF_USERTYPE')
-//     .select()
-//     .eq('id', userTypeData[0].user_type);
-
-//   console.log(userTypeData[0])
-//   // console.log(userTypeDataInt)
-//   console.log("USER TYPE OF ", userTypeData[0].first_name, " is ", userTypeDataInt[0].description)
-
-//   if (userTypeError) {
-//     console.error("Error querying user type:", userTypeError.message);
-//     return;
-//   }
-
-// }
-
-//   useEffect(() => {
-//     testFunc()
-//   }, [])
-
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-//       this is the home page.
-//     </main>
-//   );
-// }
 
 const Home = ({ userType }) => {
   return (
