7207a01ce6de23d8faf48f0b6325ce89335a24da
migzprz <134031394+migzprz@users.noreply.github.com>
2023-11-17 02:31:22 +0800

    navbar for record orders

--- app/recordorders/page.jsx
+++ app/recordorders/page.jsx
@@ -1,12 +1,16 @@
 "use client";
 
-import { useState } from "react";
 import SalesOrderList from "../components/SalesOrderList";
-import AddProductSales from "../components/AddProductSales";
+import Navbar from "../components/Navbar";
 
 const RecordOrder = () => {
+ 
   return (
+    <>
+     <Navbar userType={"Salesperson"}/>
+    
     <div className="w-full min-h-screen flex flex-col justify-center items-center gap-2" style={{ backgroundColor: '#27374D', color: 'white' }}>
+      
       <div className="w-[80%] px-10 py-4 bg-blue-400 rounded-lg" style={{ backgroundColor: '#526D82', color: 'white' }}>
         <h1 className="font-black text-xl">Record Sales Orders</h1>
       </div>
@@ -16,7 +20,7 @@ const RecordOrder = () => {
 
         
       </div>
-   
+      </>
 
     
   );
