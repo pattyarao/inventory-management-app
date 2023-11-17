803fa7a67fabfe9b80bd2f2151c4caa3dd35b89f
migzprz <134031394+migzprz@users.noreply.github.com>
2023-11-17 12:09:16 +0800

    fixed add new variant

--- app/components/AddNewVariant.jsx
+++ app/components/AddNewVariant.jsx
@@ -209,7 +209,7 @@ const AddNewVariant = (props) => {
                     >
                       Close
                     </button>
-                    <button>
+                    <button
                       className="text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                       style={{ backgroundColor: "#097969"}}
                       type="button"
