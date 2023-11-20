152e2a6a7d1bc51e78d6ae4e82e856373cffdcaf
Jerrick-Santos-DLSU <jerrickezekiel0122@gmail.com>
2023-11-20 15:23:15 +0800

    implemented SES Model and initial Forecasting mechanism

--- app/components/TableModal.jsx
+++ app/components/TableModal.jsx
@@ -5,14 +5,17 @@ const TableModal = ({ isVisible, reportData, reportType, choice, onClose, startD
   if (!isVisible) return null;
 
   return (
-    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 bg-black backdrop-filter backdrop-blur-lg">
-      <div className="bg-white p-8 rounded-md shadow-lg max-h-96 overflow-y-auto">
-        <button
+    
+    
+    <div className="fixed top-0 left-0 w-full h-screen flex items-center justify-center bg-opacity-50 bg-black backdrop-filter backdrop-blur-lg">
+                <button
           className="absolute top-4 right-4 text-white hover:text-red-500"
           onClick={onClose}
         >
           CLOSE X
         </button>
+      <div className=" w-[90%] bg-white p-8 rounded-md shadow-lg h-[80%] overflow-y-auto">
+
 
         <DetailedTable reportData={reportData} reportType={reportType} choice={choice} startDate={startDate} endDate={endDate} />
       </div>
