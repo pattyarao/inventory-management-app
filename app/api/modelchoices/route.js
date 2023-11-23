1d31de55d22028e66e3eb40326ba7a73723d3dfc
Jerrick-Santos-DLSU <jerrickezekiel0122@gmail.com>
2023-11-23 21:49:49 +0800

    fully implemented models

---
+++ app/api/modelchoices/route.js
@@ -0,0 +1,23 @@
+import supabase from "../../supabase";
+
+export async function GET() {
+  const { data: models, error } = await supabase
+    .from("ML_MODELS")
+    .select("*");
+
+  if (error) {
+    return new Response(JSON.stringify({ error }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  const json = {
+    models: models,
+  };
+
+  return new Response(JSON.stringify(json), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
