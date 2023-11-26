ef662966620b34b539929ed9ab21218357b6ffd5
Patty Arao <patricia_arao@dlsu.edu.ph>
2023-11-26 14:57:06 +0800

    added loading component

--- app/assignroles/page.jsx
+++ app/assignroles/page.jsx
@@ -1,16 +1,15 @@
 "use client";
 
 import AddEmployee from "../components/AddEmployee";
-import EmployeeList from "../components/EmployeeList";
+import EmployeeList from "../components/EmployeeList"
+import Loader from "../components/Loader";
 import Navbar from "../components/Navbar";
 import withAuthentication from "../auth";
 
 const AssignRoles = ({ userType, userInfo }) => {
   if (!userType) {
     return (
-      <div>
-        <p>Loading</p>
-      </div>
+      <Loader/>
     );
   }
   return (
