cc1b971c3be4c4a89c3c05fa518a18ac4abfe2a7
migzprz <134031394+migzprz@users.noreply.github.com>
2023-11-27 18:17:38 +0800

    trial resolving for discrespancy detailed

--- app/api/detaileddiscrepancy/route.js
+++ app/api/detaileddiscrepancy/route.js
@@ -1,27 +1,38 @@
 import supabase from "../../supabase";
 
 export async function GET(start_date, end_date, selectedOptions) {
-
+  try {
     const { data: detailedReport, error } = await supabase
-    .from("DETAILED_DISCREPANCY")
-    .select()
-    .gte('transaction_date', start_date) 
-    .lt('transaction_date', end_date)
-    .in('material_name', selectedOptions)
+      .from("DETAILED_DISCREPANCY")
+      .select()
+      .gte('transaction_date', start_date) 
+      .lt('transaction_date', end_date)
+      .in('material_name', selectedOptions);
 
-  if (error) {
-    return new Response(JSON.stringify({ error }), {
+    if (error) {
+      throw error;
+    }
+
+    if (Array.isArray(detailedReport)) {
+      const json = {
+        detailedReport: detailedReport,
+      };
+
+      return new Response(JSON.stringify(json), {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
+    } else {
+      // Handle the case where detailedReport is not an array
+      throw new Error("detailedReport is not an array");
+    }
+  } catch (error) {
+    // Handle other errors, e.g., network issues, Supabase client setup, etc.
+    console.error("Error fetching detailed report:", error);
+
+    return new Response(JSON.stringify({ error: "Internal server error" }), {
       status: 500,
       headers: { "Content-Type": "application/json" },
     });
   }
-
-  const json = {
-    detailedReport: detailedReport,
-  };
-
-  return new Response(JSON.stringify(json), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
 }
