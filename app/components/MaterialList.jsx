0c9553ff80e2f8c220b4554bf2e7a649d23906d1
w-choudai <danielle_marie_pua@dlsu.edu.ph>
2023-11-27 22:06:35 +0800

    modified:   app/components/MaterialList.jsx 	modified:   app/materialstocklevels/page.jsx

--- app/components/MaterialList.jsx
+++ app/components/MaterialList.jsx
@@ -3,14 +3,12 @@
 import { useState, useEffect } from "react";
 import { GET as GETMaterialMaster } from '../api/materialmaster/route';
 import Loader from "../components/Loader";
-// import { GET as GETForecast } from '../api/forecast/route';
 
-
-const MaterialList = ({ searchTerm, view, sortOption, selected_model }) => {
+const MaterialList = ({ searchTerm, view, sortOption, selected_model, onLoadingChange }) => {
   const [materials, setMaterials] = useState([]);
-  const [filterOption, setFilterOption] = useState('All'); // Default filter option
+  const [filterOption, setFilterOption] = useState('All');
   const [prediction, setPrediction] = useState([]);
-  const [loading, setLoading] = useState(false); // loading state
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function getMaterials() {
@@ -62,29 +60,31 @@ const MaterialList = ({ searchTerm, view, sortOption, selected_model }) => {
     //   }
     // }
 
-      async function forecast() {
-        setLoading(true);
-  
-        try {
-          const response = await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/api/forecast/' + selected_model);
-  
-          if (response.status === 200) {
-            const data = await response.json();
-            setPrediction(data.data);
-          } else {
-            console.error('API Error:', response);
-          }
-        } catch (error) {
-          console.error('Error fetching forecast:', error);
-        } finally {
-          setLoading(false);
-        }
-      }
-  
-      forecast();
-    }, [selected_model]);
+    async function forecast() {
+      setLoading(true);
 
-  useEffect (() => console.log(prediction), [prediction])
+      try {
+        const response = await fetch(process.env.NEXT_PUBLIC_BASE_URL + "/api/forecast/" + selected_model);
+
+        if (response.status === 200) {
+          const data = await response.json();
+          setPrediction(data.data);
+        } else {
+          console.error("API Error:", response);
+        }
+      } catch (error) {
+        console.error("Error fetching forecast:", error);
+      } finally {
+        setLoading(false);
+        // Call the callback function to update loading state in MaterialStockLevel
+        onLoadingChange && onLoadingChange(false);
+      }
+    }
+
+    forecast();
+  }, [selected_model, onLoadingChange]);
+  
+    useEffect(() => console.log(prediction), [prediction]);  
 
   // Filter materials based on the search term and filter option
   const filteredMaterials = materials.filter((material) => {
@@ -141,79 +141,85 @@ const MaterialList = ({ searchTerm, view, sortOption, selected_model }) => {
 
   return (
     <div className="mb-2 bg-[#D6E0F0] p-4 flex flex-col rounded-md">
+      {/* Display loader if loading is true */}
+      {loading && <Loader />}
+  
       {/* Headings and Filter dropdown */}
-      <div
-        className={`${
-          view === 'grid'
-            ? 'grid grid-cols-4 gap-4 items-center'
-            : 'flex justify-between items-center'
-        } bg-[#D6E0F0] rounded-lg p-4 `}
-      >
-        <h1 className="pl-10 w-1/5 font-black text-xl text-black">
-          Material
-        </h1>
-        <h1 className="pl-10 w-1/5 whitespace-normal text-left font-black text-xl text-black">
-          Current Stock
-        </h1>
-        <h1 className="pl-10 w-1/5 whitespace-normal text-center font-black text-xl text-black">
-          Suggested Restock Amount
-        </h1>
-        <div className="flex items-center">
-        {loading && (
-        <div className="flex items-right">
-          <Loader />
+      {!loading && (
+        <div
+          className={`${
+            view === 'grid'
+              ? 'grid grid-cols-4 gap-4 items-center'
+              : 'flex justify-between items-center'
+          } bg-[#D6E0F0] rounded-lg p-4 `}
+        >
+          <h1 className="pl-10 w-1/5 font-black text-xl text-black">
+            Material
+          </h1>
+          <h1 className="pl-10 w-1/5 whitespace-normal text-left font-black text-xl text-black">
+            Current Stock
+          </h1>
+          <h1 className="pl-10 w-1/5 whitespace-normal text-center font-black text-xl text-black">
+            Suggested Restock Amount
+          </h1>
+          <select
+            className="p-3 ml-2 rounded-md bg-white"
+            value={filterOption}
+            onChange={handleFilterChange}
+          >
+            <option value="All">All Materials</option>
+            <option value="qtyIsZero">Materials with No Stock</option>
+            <option value="predictedValue">Materials for Restocking</option>
+            <option value="stockIsSufficient">Sufficient Stock</option>
+            <option value="N/A">Insufficient Data for Prediction</option>
+          </select>
         </div>
       )}
-        </div>
-        <select
-          className="p-3 ml-2 rounded-md bg-white"
-          value={filterOption}
-          onChange={handleFilterChange}
-        >
-          <option value="All">All Materials</option>
-          <option value="qtyIsZero">Materials with No Stock</option>
-          <option value="predictedValue">Materials for Restocking</option>
-          <option value="stockIsSufficient">Sufficient Stock</option>
-          <option value="N/A">Insufficient Data for Prediction</option>
-        </select>
-      </div>
+  
       {/* Render the filtered and sorted materials */}
-      <div className="m-auto w-full">
-        <div className="flex flex-col gap-4">
-          {sortedMaterials.map((material, index) => (
-            <div id={material.id} key={index} className=" flex flex-col rounded-md">
-              <div
-                className={`${
-                  view === 'grid'
-                    ? 'grid grid-cols-4 gap-4 items-center'
-                    : 'flex justify-between items-center'
-                } bg-[#F1F3F8] rounded-lg`}
-              >
-                <p className="pl-10 text-left font-semibold w-1/5 whitespace-normal">
-                  {material.name}</p>
-                <p className="p-6 text-right font-semibold w-2/5 whitespace-normal">
-                  {material.qty_available}
-                  {material.REF_METRIC.metric_unit}
-                </p>
-                <p className="p-6 font-semibold text-right w-2/5 whitespace-normal">
-                  {(prediction[material.id] != null) ? (
-                    prediction[material.id] === "N/A"
-                      ? "Insufficient Data for Prediction"
-                      : prediction[material.id] === "Stock is Sufficient"
-                        ? "Stock is Sufficient"
-                        : `${prediction[material.id]} ${material.REF_METRIC.metric_unit || ""}`
-                  ) : "No Data"}
-                </p>
-                <p
-                  className={`w-flex p-2 text-right font-semibold w-2/5 whitespace-normal rounded-md rounded-tl-none rounded-bl-none ${
-                    'bg-[#F1F3F8]'
-                  }`}
-                ></p>
+      {!loading && (
+        <div className="m-auto w-full">
+          <div className="flex flex-col gap-4">
+            {sortedMaterials.map((material, index) => (
+              <div id={material.id} key={index} className="flex flex-col rounded-md">
+                <div
+                  className={`${
+                    view === 'grid'
+                      ? 'grid grid-cols-4 gap-4 items-center'
+                      : 'flex justify-between items-center'
+                  } bg-[#F1F3F8] rounded-lg`}
+                >
+                  <p className="pl-10 text-left font-semibold w-1/5 whitespace-normal">
+                    {material.name}
+                  </p>
+                  <p className="p-6 text-right font-semibold w-2/5 whitespace-normal">
+                    {material.qty_available}
+                    {material.REF_METRIC.metric_unit}
+                  </p>
+                  <p className="p-6 font-semibold text-right w-2/5 whitespace-normal">
+                    {prediction[material.id] != null ? (
+                      prediction[material.id] === 'N/A' ? (
+                        'Insufficient Data for Prediction'
+                      ) : prediction[material.id] === 'Stock is Sufficient' ? (
+                        'Stock is Sufficient'
+                      ) : (
+                        `${prediction[material.id]} ${material.REF_METRIC.metric_unit || ''}`
+                      )
+                    ) : (
+                      'No Data'
+                    )}
+                  </p>
+                  <p
+                    className={`w-flex p-2 text-right font-semibold w-2/5 whitespace-normal rounded-md rounded-tl-none rounded-bl-none ${
+                      'bg-[#F1F3F8]'
+                    }`}
+                  ></p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
